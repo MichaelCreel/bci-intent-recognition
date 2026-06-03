4e5b56c7830891345519807c730d6a251f46b4be
Michael Creel <michaelcreel815@gmail.com>
2026-06-03 10:00:01 -0600

    Added Titles to Plots

--- notebooks/week_2/physionet_data.py
+++ notebooks/week_2/physionet_data.py
@@ -15,15 +15,15 @@ print(f"Subject 1 type: {type(data[1])}")
 
 subject_data = data[1]
 
-# Raw data
+# Raw data from run 0, session 0
 raw = subject_data['0']['0']
-raw.plot()
+fig = raw.plot()
+fig.suptitle("Raw EEG Data - Subject 1 - Run 0 - Session 0")
 
-# Filtered Data
+# Filtered Data between 8 and 30 Hz
 raw_filtered = raw.copy().filter(8, 30)
-raw_filtered.plot()
-
-print("\n\n")
+fig = raw_filtered.plot()
+fig.suptitle("Filtered EEG Data - Subject 1 - Run 0 - Session 0")
 
 # Extract events
 events, event_id = mne.events_from_annotations(raw)
@@ -42,28 +42,36 @@ epochs = mne.Epochs(
 )
 
 # Plot epochs
-epochs.plot()
+fig = epochs.plot()
+fig.suptitle("Epochs - Subject 1 - Run 0 - Session 0")
     
 # Plot epoch for C3
-epochs.plot(picks="C3")
+fig = epochs.plot(picks="C3")
+fig.suptitle("Epochs - Subject 1 - Run 0 - Session 0 - C3")
 
 # Plot PSD
-raw.plot_psd(fmax = 40)
+fig = raw.plot_psd(fmax = 40)
+fig.suptitle("PSD - Subject 1 - Run 0 - Session 0")
 
 # Perform high pass on PSD and plot
 raw_hp = raw.copy().filter(1., None)
-raw_hp.plot_psd(fmax = 40)
+fig = raw_hp.plot_psd(fmax = 40)
+fig.suptitle("High Pass PSD - Subject 1 - Run 0 - Session 0")
 
 # Retrieve PSD for left and right hands
 epochs_left = epochs['left_hand']
 epochs_right = epochs['right_hand']
 
 # Plot C3 PSDs for left and right hand
-epochs_left.plot_psd(picks="C3", fmax=40)
-epochs_right.plot_psd(picks="C3", fmax=40)
+fig = epochs_left.plot_psd(picks="C3", fmax=40)
+fig.suptitle("C3 PSD - Left Hand - Subject 1 - Run 0 - Session 0")
+fig = epochs_right.plot_psd(picks="C3", fmax=40)
+fig.suptitle("C3 PSD - Right Hand - Subject 1 - Run 0 - Session 0")
 
 # Plot C4 PSDs for left and right hand
-epochs_left.plot_psd(picks="C4", fmax=40)
-epochs_right.plot_psd(picks="C4", fmax=40)
+fig = epochs_left.plot_psd(picks="C4", fmax=40)
+fig.suptitle("C4 PSD - Left Hand - Subject 1 - Run 0 - Session 0")
+fig = epochs_right.plot_psd(picks="C4", fmax=40)
+fig.suptitle("C4 PSD - Right Hand - Subject 1 - Run 0 - Session 0")
 
 mne.viz.utils.plt_show()
