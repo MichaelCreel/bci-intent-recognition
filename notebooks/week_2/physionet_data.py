5c5b7fc8357457234ad3327ec8a0a01543d6ede8
Michael Creel <michaelcreel815@gmail.com>
2026-06-03 10:12:45 -0600

    Added Figure Saving to Week 2 Physionet Data Script & Added Generated Figures Text File to Week 2 Figures

--- notebooks/week_2/physionet_data.py
+++ notebooks/week_2/physionet_data.py
@@ -5,6 +5,15 @@
 from moabb.datasets import PhysionetMI
 import mne
 
+generated_figs = []
+
+# Save figures to figs/week_2/
+def save_figure(fig, filename):
+    fig.savefig(f"figs/week_2/{filename}.png")
+    generated_figs.append(f"{filename}.png")
+    with open("figs/week_2/generated_figs.txt", "w") as f:
+        f.write("\n".join(generated_figs))
+
 dataset = PhysionetMI()
 
 # Load data for subject 1
@@ -19,11 +28,13 @@ subject_data = data[1]
 raw = subject_data['0']['0']
 fig = raw.plot()
 fig.suptitle("Raw EEG Data - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "Raw_EEG_Data_Subject_1_Run_0_Session_0")
 
 # Filtered Data between 8 and 30 Hz
 raw_filtered = raw.copy().filter(8, 30)
 fig = raw_filtered.plot()
 fig.suptitle("Filtered EEG Data - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "Filtered_EEG_Data_Subject_1_Run_0_Session_0")
 
 # Extract events
 events, event_id = mne.events_from_annotations(raw)
@@ -44,19 +55,23 @@ epochs = mne.Epochs(
 # Plot epochs
 fig = epochs.plot()
 fig.suptitle("Epochs - Subject 1 - Run 0 - Session 0")
-    
+save_figure(fig, "Epochs_Subject_1_Run_0_Session_0")
+
 # Plot epoch for C3
 fig = epochs.plot(picks="C3")
 fig.suptitle("Epochs - Subject 1 - Run 0 - Session 0 - C3")
+save_figure(fig, "Epochs_Subject_1_Run_0_Session_0_C3")
 
 # Plot PSD
 fig = raw.plot_psd(fmax = 40)
 fig.suptitle("PSD - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "PSD_Subject_1_Run_0_Session_0")
 
 # Perform high pass on PSD and plot
 raw_hp = raw.copy().filter(1., None)
 fig = raw_hp.plot_psd(fmax = 40)
 fig.suptitle("High Pass PSD - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "High_Pass_PSD_Subject_1_Run_0_Session_0")
 
 # Retrieve PSD for left and right hands
 epochs_left = epochs['left_hand']
@@ -65,13 +80,17 @@ epochs_right = epochs['right_hand']
 # Plot C3 PSDs for left and right hand
 fig = epochs_left.plot_psd(picks="C3", fmax=40)
 fig.suptitle("C3 PSD - Left Hand - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "C3_PSD_Left_Hand_Subject_1_Run_0_Session_0")
 fig = epochs_right.plot_psd(picks="C3", fmax=40)
 fig.suptitle("C3 PSD - Right Hand - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "C3_PSD_Right_Hand_Subject_1_Run_0_Session_0")
 
 # Plot C4 PSDs for left and right hand
 fig = epochs_left.plot_psd(picks="C4", fmax=40)
 fig.suptitle("C4 PSD - Left Hand - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "C4_PSD_Left_Hand_Subject_1_Run_0_Session_0")
 fig = epochs_right.plot_psd(picks="C4", fmax=40)
 fig.suptitle("C4 PSD - Right Hand - Subject 1 - Run 0 - Session 0")
+save_figure(fig, "C4_PSD_Right_Hand_Subject_1_Run_0_Session_0")
 
 mne.viz.utils.plt_show()
