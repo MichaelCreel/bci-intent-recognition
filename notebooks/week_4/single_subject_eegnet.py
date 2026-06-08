07f8734bb70980ac9501ed9e134c7f74a074c49f
Michael Creel <michaelcreel815@gmail.com>
2026-06-08 11:20:43 -0600

    Added Missing Space

--- notebooks/week_4/single_subject_eegnet.py
+++ notebooks/week_4/single_subject_eegnet.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
 
     for subject, matrix in confusion_matrices:
         plt.imshow(matrix, cmap = "Blues")
-        plt.title(f"Single SubjectConfusion Matrix - Subject {subject}")
+        plt.title(f"Single Subject Confusion Matrix - Subject {subject}")
         plt.xlabel("Predicted")
         plt.ylabel("True")
         plt.colorbar()
